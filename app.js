aab059bd8f6dfc146941febe0cada302a3c675a5
Aldo320 <aldoantunez12@gmail.com>
2022-10-10 13:48:50 -0300

    first commit

--- app.js
+++ app.js
@@ -1,89 +1,85 @@
-const Championes = [
-  {id: 1, producto:"Nike AirMax", precio: 5490, imagen: '../assets/img/Nike-logo.jpg'},
-  {id: 2, producto:"Adidas Coreracer", precio: 4290, imagen: '../assets/img/historia-origen-marcas-adidas.jpg'},
-  {id: 3, producto:"Puma Xray Deportivo", precio: 4990, imagen: '../assets/img/PUMA-logo.jpg'},
-  {id: 4, producto:"Reebok Triplehall", precio: 2880, imagen: '../assets/img/Reebok.jpg' },
-  {id: 5, producto:"New Balance Lifestyle", precio: 3990, imagen: '../assets/img/New-Balance-logo.jpg'},
-  {id: 6, producto:"York Umbro", precio: 4150, imagen: '../assets/img/Umbro-Logo-1999.jpg'},
-]
-
-const [Nike,Adidas,Puma,Reebok,York,Umbro]= Championes
-console.log (Nike)
-
 let carrito = [];
 const divisa = '$';
 const DOMitems = document.querySelector('#items');
 const DOMcarrito = document.querySelector('#carrito');
 const DOMtotal = document.querySelector('#total');
 const botonComprar = document.querySelector('#boton-comprar');
+const botonSorteo = document.querySelector('#boton-sorteo');
 const formulario = document.querySelector('container') 
-botonComprar.onclick = function() {
-  alert("Tu Compra Ha Sido Realizada Exitosamente")
-};
+
 
 
 
 function renderizarProductos() {
-  Championes.forEach((info) => {
-      const Clase = document.createElement('div');
-      Clase.classList.add('card', 'col-sm-4');
-      const CardBody = document.createElement('div');
-      CardBody.classList.add('card-body');
-      const Titulo = document.createElement('h5');
-      Titulo.classList.add('card-title');
-      Titulo.textContent = info.producto;
-      const Imagen = document.createElement('img');
-      Imagen.classList.add('img-fluid');
-      Imagen.setAttribute('src', info.imagen);
-      const Precio = document.createElement('p');
-      Precio.classList.add('card-text');
-      Precio.textContent = `${info.precio}${divisa}`;
-      const Boton = document.createElement('button');
-      Boton.classList.add('btn', 'btn-primary');
-      Boton.textContent = 'Agregar Al Carrito';
-      Boton.setAttribute('marcador', info.id);
-      Boton.addEventListener('click', anyadirProductoAlCarrito);
-      CardBody.appendChild(Imagen);
-      CardBody.appendChild(Titulo);
-      CardBody.appendChild(Precio);
-      CardBody.appendChild(Boton);
-      Clase.appendChild(CardBody);
-      DOMitems.appendChild(Clase);
-  });
-}
+  fetch("../data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((info)=>{
+        const Clase=document.createElement("div"); 
+        Clase.classList.add("card','col-sm-4"); 
+        const CardBody=document.createElement("div");   
+        CardBody.classList.add("card-body"); 
+        const Titulo =document.createElement("h5"); 
+        Titulo.classList.add("card-title"); 
+        Titulo.textContent=info.producto; 
+        const Imagen=document.createElement("img"); 
+        Imagen.classList.add("img-fluid"); 
+        Imagen.setAttribute("src",info.imagen); 
+        const Precio=document.createElement("p"); 
+        Precio.classList.add("card-text"); 
+        Precio.textContent=`${info.precio}${divisa}`; 
+        const Boton=document.createElement("button"); 
+        Boton.classList.add("btn","btn-primary"); 
+        Boton.textContent="Agregar Al Carrito"; 
+        Boton.setAttribute("marcador",info.id); 
+        Boton.addEventListener("click",aniadirProductoAlCarrito); 
+        CardBody.appendChild(Imagen); 
+        CardBody.appendChild(Titulo); 
+        CardBody.appendChild(Precio); 
+        CardBody.appendChild(Boton); 
+        Clase.appendChild(CardBody); 
+        DOMitems.appendChild(Clase); 
+      });         
+      }); 
+    }
 
-
-function anyadirProductoAlCarrito(evento) {
+function aniadirProductoAlCarrito(evento) {
   carrito.push(evento.target.getAttribute('marcador'))
   renderizarCarrito();
 
 }
 
+
 function renderizarCarrito() {
-  DOMcarrito.textContent = '';
+  DOMcarrito.textContent = "";
   const carritoSinDuplicados = [...new Set(carrito)];
   carritoSinDuplicados.forEach((item) => {
-      const miItem = Championes.filter((itemChampiones) => {
+    fetch("../data.json")
+    .then((response)=>response.json())
+    .then((data)=>{
+      const miItem = data.filter((itemChampiones) => {
           return itemChampiones.id === parseInt(item);
       });
       const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-          return itemId === item ? total += 1 : total;
+          return itemId === item ? (total += 1) : total;
       }, 0);
-      const Clase = document.createElement('li');
-      Clase.classList.add('list-group-item', 'text-right', 'mx-2');
+      const Clase = document.createElement("li");
+      Clase.classList.add("list-group-item", "text-right", "mx-2");
       Clase.textContent = `${numeroUnidadesItem} x ${miItem[0].producto} - ${miItem[0].precio}${divisa}`;
-      const miBoton = document.createElement('button');
-      miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-      miBoton.textContent = 'X';
-      miBoton.style.marginLeft = '1rem';
+      const miBoton = document.createElement("button");
+      miBoton.classList.add("btn", "btn-danger", "mx-5");
+      miBoton.textContent = "X";
+      miBoton.style.marginLeft = "1rem";
       miBoton.dataset.item = item;
-      miBoton.addEventListener('click', comprarItemCarrito);
+      miBoton.addEventListener("click", comprarItemCarrito);
      
       Clase.appendChild(miBoton);
       DOMcarrito.appendChild(Clase);
-  });
+    });
   DOMtotal.textContent = calcularTotal();
+});
 }
+
 
 function comprarItemCarrito(evento) {
   const id = evento.target.dataset.item;
